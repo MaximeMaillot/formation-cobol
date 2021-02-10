d887aa12ade16982fa8e09e077f3637bdc34a0cb
MaximeMaillot <maillot.maxime62@gmail.com>
2021-02-10 14:51:13 +0100

    fix project
    sub program now only retrieve the error message and no longer write it
    Use flag to handle f-error
    Moved etatano write paragraph to the main program
    Use a call to the sub program to get the error message for stats
    Format correctly stats and etatano

--- ProjetCobolVsam/ETATANO.cob
+++ ProjetCobolVsam/ETATANO.cob
@@ -11,15 +11,13 @@
             ACCESS MODE IS RANDOM
             RECORD KEY IS error-key-x
             FILE STATUS IS CR-ERRVS.
-           SELECT f-etatano ASSIGN ETATANO
-            FILE STATUS IS CR-ETATANO.
 
       *********************************
       *    D A T A   D I V I S I O N
       *********************************
        DATA DIVISION.
        FILE SECTION.
-       fd f-error is external
+       fd f-error
            DATA RECORD IS e-error.
        01 e-error.
            02 error-key-9          pic 9(3).
@@ -28,40 +26,41 @@
            02 err-message          pic x(60).
            02                      PIC X(17).
 
-       fd f-etatano is external.
-       01 etatano.
-           COPY CANO.
-
 
        WORKING-STORAGE SECTION.
        
        01 CR-ERRVS                 PIC 99.
-       01 CR-ETATANO               PIC 99.
 
        LINKAGE SECTION.
-       01 L-CODE-MVT               PIC X.
-       01 L-MATRICULE              PIC X(6).
        01 L-ERROR-CODE             PIC 9(3).
+       01 flag                     PIC 9.
+       01 err-label                PIC x(60).
 
-       PROCEDURE DIVISION USING L-CODE-MVT L-MATRICULE L-ERROR-CODE.
-           PERFORM 10000-INIT-PGM
-           PERFORM 20000-TRAITEMENT
-           PERFORM 30000-END-PGM
+       PROCEDURE DIVISION 
+           USING L-ERROR-CODE FLAG err-label.
+           EVALUATE FLAG
+            WHEN 1
+              PERFORM 10000-INIT-PGM
+            WHEN 5
+              PERFORM 20000-TRAITEMENT
+            WHEN 9
+              PERFORM 30000-END-PGM
+            WHEN OTHER
+              GOBACK
+           END-EVALUATE
+           GOBACK
            .
 
        10000-INIT-PGM.
-           continue
+           OPEN INPUT f-error
            .
 
        20000-TRAITEMENT.
            MOVE L-ERROR-CODE TO error-key-9
            READ f-error
-           MOVE L-MATRICULE to NUM-MAT
-           MOVE L-CODE-MVT TO CODE-MVT-ANO
-           MOVE err-message TO LIB-MESS
-           WRITE etatano
+           MOVE err-message TO err-label
            .
            
        30000-END-PGM.
-           GOBACK
+           CLOSE f-error
            .     
--- ProjetCobolVsam/PROJCVS1.cob
+++ ProjetCobolVsam/PROJCVS1.cob
@@ -24,12 +24,6 @@
             ACCESS MODE SEQUENTIAL 
             RECORD KEY IS MAT-X4 
             FILE STATUS IS CR-ASSURES-OUT.
-
-           SELECT f-error ASSIGN ERRVS
-            ORGANIZATION IS INDEXED
-            ACCESS MODE IS RANDOM
-            RECORD KEY IS error-key-x
-            FILE STATUS IS CR-ERRVS.
            
            SELECT f-etatano ASSIGN ETATANO
             FILE STATUS IS CR-ETATANO.
@@ -57,18 +51,10 @@
        fd f-assures-out.
        01 assures-out.
            COPY CASSURE4.
-       
-       fd f-error is external
-           DATA RECORD IS e-error.
-       01 e-error.
-           02 error-key-9                pic 9(3).
-           02 error-key-x 
-            REDEFINES error-key-9        PIC x(3).
-           02 err-message                pic x(60).
-           02                            PIC X(17).
 
-       fd f-etatano is external.
-       01 etatano                        PIC X(80).
+       fd f-etatano.
+       01 etatano.
+           COPY CANO.
        
 
        WORKING-STORAGE SECTION.
@@ -108,9 +94,109 @@
 
        77 ano-pgm                        PIC X(7) VALUE 'ETATANO'.
 
-       77 I                              PIC 99.
+       77 I                              PIC 999.
 
-       77 ERR-CODE                       PIC 99.
+       77 ERR-CODE                       PIC 999.
+
+       01 weekday-desc.
+         COPY weekday.
+       01 weekday-tab.
+         05 weekday PIC X(10) occurs 7.
+
+       01 DATE-F.
+         05 YEAR-F PIC 9999.
+         05 MONTH-F PIC 99.
+         05 DAY-F PIC 99.
+       01 TIME-F.
+         05 HOUR-F PIC 99.
+         05 MINUTE-F PIC 99.
+         05 SECOND-F PIC 99.
+       01 WEEKDAY-F PIC 9.
+
+      * -------------------- FORMAT ANO ----------------
+       01 FORMAT-HEADER-TITLE-ANO.
+         05 PIC X(31).
+         05 PIC X(18) value 'ETAT DES ANOMALIES'.
+         05 PIC X(31).
+       
+       01 FORMAT-MAJ-ANO.
+         05 PIC X(14) value 'MISE A JOUR DU'.
+         05 PIC X(3) value ' : '.
+         05 PIC X(3).
+         05 DAY-F PIC 9(2).
+         05 PIC X value '/'.
+         05 MONTH-F PIC 9(2).
+         05 PIC X value '/'.
+         05 YEAR-F PIC 9(4).
+         05 PIC X(50).
+       01 FORMAT-GROUPE-ANO.
+         05 PIC X(14) value 'GROUPE'.
+         05 PIC X(3) value ' : '.
+         05 PIC X(3).
+         05 PIC X(10) value 'API11'.
+         05 PIC X(50).
+
+       01 FORMAT-EMPTY-LIGNE-ANO.
+         05 PIC X(80) value space.
+         
+       01 FORMAT-LIGNE-TAB-ANO.
+         05 PIC X(80) value all '-'.
+
+      * ------------------------------------------------ 
+
+      * -------------------- FORMAT STATS --------------
+
+       01 FORMAT-HEADER-STATS-1.
+         05 PIC X.
+         05 PIC X(7) value 'User : '.
+         05 PIC X(10) value 'API11'.
+         05 PIC X(28).
+         05 WEEKDAY-S PIC X(10).
+         05 PIC X(4) value ' Le '.
+         05 DAY-F PIC 9(2).
+         05 PIC X value '/'.
+         05 MONTH-F PIC 9(2).
+         05 PIC X value '/'.
+         05 YEAR-F PIC 9(4).
+       
+       01 FORMAT-HEADER-STATS-2.
+         05 PIC X(65).
+         05 HOUR-F PIC 9(2).
+         05 PIC X value ':'.
+         05 MINUTE-F PIC 9(2).
+      
+       01 FORMAT-TITLE-STATS.
+         05 PIC X value '|'.
+         05 PIC X(24).
+         05 PIC X(12) value 'STATISTIQUES'.
+         05 PIC X(34).
+         05 PIC X value '|'.
+
+       01 FORMAT-ENRGMT-STATS.
+         05 PIC X value 'I'.
+         05 PIC X.
+         05 message-erreur PIC X(60).
+         05 PIC X(6).
+         05 CPTI-F PIC 99.
+         05 PIC X.
+         05 PIC X value 'I'.
+
+       01 FORMAT-EMPTY-LIGNE-STATS.
+        05 PIC X value 'I'.
+        05 PIC X(70).
+        05 PIC X value 'I'.
+
+       01 FORMAT-LIGNE-TABLE-STATS.
+        05 PIC X value '+'.
+        05 PIC x(70) value all '-'.
+        05 pic x value '+'.
+
+      * -------------------------------------------------
+
+       01 FLAG.
+         05 FLAG-OPEN PIC 9 value 1.
+         05 FLAG-CONTINUE PIC 9 value 5.
+         05 FLAG-CLOSE PIC 9 value 9.
 
       ****************************************************************
       * P R O C E D U R E   D I V I S I O N
@@ -122,12 +208,18 @@
            .
 
        10000-INIT-PGM.
-           OPEN INPUT f-assures-in f-mvt f-error
-           OPEN OUTPUT f-assures-out
-           OPEN EXTEND f-etatano 
+           OPEN INPUT f-assures-in f-mvt 
+           OPEN OUTPUT f-assures-out f-etatano
            perform 11000-CHECK-INIT-FILE
-           perform READ-ASSURES-IN
-           perform READ-MVT
+           perform 18000-READ-ASSURES-IN
+           perform 19000-READ-MVT
+           CALL ano-pgm USING
+                BY value 0
+                BY CONTENT FLAG-OPEN
+                BY content space
+
+           perform get-current-date
+           perform write-ano-header
            .
 
        11000-CHECK-INIT-FILE.
@@ -151,7 +243,28 @@
            END-IF
            .
        
-       READ-ASSURES-IN.
+       GET-CURRENT-DATE.
+           ACCEPT DATE-F FROM DATE YYYYMMDD
+           ACCEPT TIME-F FROM TIME
+           ACCEPT WEEKDAY-F FROM DAY-OF-WEEK
+           .
+
+       write-ano-header.
+           MOVE CORRESPONDING DATE-F TO FORMAT-MAJ-ANO
+
+           WRITE etatano from FORMAT-HEADER-TITLE-ANO
+           WRITE etatano from FORMAT-EMPTY-LIGNE-ANO
+           WRITE etatano from FORMAT-MAJ-ANO
+           WRITE etatano from FORMAT-GROUPE-ANO
+           WRITE etatano from FORMAT-EMPTY-LIGNE-ANO
+           WRITE etatano from FORMAT-LIGNE-TAB-ANO
+           .
+       
+       write-ano-footer.
+           write etatano from FORMAT-LIGNE-TAB-ANO
+           .
+
+       18000-READ-ASSURES-IN.
            READ f-assures-in
 
            IF NOT EOF-ASSURES-IN 
@@ -159,7 +272,7 @@
            END-IF
            .
        
-       READ-MVT.
+       19000-READ-MVT.
            READ f-mvt
 
            IF NOT EOF-MVT
@@ -171,96 +284,125 @@
            perform until EOF-MVT
 
              perform until MAT-MVT <= MAT-A3 OR EOF-ASSURES-IN
-               perform WRITE-ASSURES-NO-MVT
-               perform READ-ASSURES-IN
+               perform 21000-WRITE-ASSURES-NO-MVT
+               perform 18000-READ-ASSURES-IN
              END-PERFORM
 
              IF (EOF-ASSURES-IN)
-               perform FINISH-MVT
+               perform 23000-FINISH-MVT
              ELSE 
-               perform EVALUATE-CODE-MVT
-               perform READ-MVT
+               perform 22000-EVALUATE-CODE-MVT
+               perform 19000-READ-MVT
              END-IF
 
            END-PERFORM
            .
        
-       CALL-ANO-PGM.
+       22700-CALL-ANO-PGM-ANO.
            CALL ano-pgm USING
-                BY REFERENCE CODE-MVT MAT-MVT ERR-CODE
+                BY REFERENCE ERR-CODE
+                BY CONTENT FLAG-CONTINUE
+                BY REFERENCE LIB-MESS
                 
+           perform 22710-WRITE-ETAT-ANO
+
            ADD 1 TO CPT-ANO-T(ERR-CODE)
            .
        
-       EVALUATE-CODE-MVT.
+       31700-CALL-ANO-PGM-STATS.
+           CALL ano-pgm USING
+                BY REFERENCE I
+                BY CONTENT FLAG-CONTINUE
+                BY REFERENCE message-erreur
+           .
+       
+       22000-EVALUATE-CODE-MVT.
            EVALUATE CODE-MVT
+
              WHEN 'C'
-               perform CHECK-WRITE-MVT
+               perform 22100-CHECK-WRITE-MVT
+
              WHEN 'M'
-               perform CHECK-REWRITE-MVT
+               perform 22200-CHECK-REWRITE-MVT
+
              WHEN 'S'
-               perform CHECK-DELETE-MVT
+               perform 22300-CHECK-DELETE-MVT
+               
              WHEN OTHER
                MOVE 1 TO ERR-CODE
-               perform CALL-ANO-PGM
+               perform 22700-CALL-ANO-PGM-ANO
+
            END-EVALUATE
            .
+       
+       22710-WRITE-ETAT-ANO.
+           MOVE MAT-MVT TO NUM-MAT
+           MOVE CODE-MVT TO CODE-MVT-ANO
+           write etatano
+           .
 
-       FINISH-MVT.
+       23000-FINISH-MVT.
            perform until EOF-MVT
 
              EVALUATE CODE-MVT
+
                WHEN 'C'
-                 perform WRITE-ASSURES-OUT
+                 perform 22110-WRITE-ASSURES-OUT
+
                WHEN 'M'
                  MOVE 3 TO ERR-CODE
-                 perform CALL-ANO-PGM
+                 perform 22700-CALL-ANO-PGM-ANO
+
                WHEN 'S'
                  MOVE 4 TO ERR-CODE
-                 perform CALL-ANO-PGM
+                 perform 22700-CALL-ANO-PGM-ANO
+
                WHEN OTHER
                  MOVE 1 TO ERR-CODE
-                 perform CALL-ANO-PGM
+                 perform 22700-CALL-ANO-PGM-ANO
+
              END-EVALUATE
-             perform READ-MVT
+
+             perform 19000-READ-MVT
 
            END-PERFORM
            .
 
-       CHECK-WRITE-MVT.
+       22100-CHECK-WRITE-MVT.
            IF (MAT-A3 NOT = MAT-MVT)
-             perform WRITE-ASSURES-OUT
+             perform 22110-WRITE-ASSURES-OUT
            ELSE
              MOVE 2 TO ERR-CODE
-             perform CALL-ANO-PGM
+             perform 22700-CALL-ANO-PGM-ANO
            END-IF 
            .
        
-       CHECK-REWRITE-MVT.
+       22200-CHECK-REWRITE-MVT.
            IF (MAT-A3 = MAT-MVT)
-             perform REWRITE-ASSURES-OUT
+             perform 22210-REWRITE-ASSURES-OUT
            ELSE
              MOVE 3 TO ERR-CODE
-             perform CALL-ANO-PGM
+             perform 22700-CALL-ANO-PGM-ANO
            END-IF
            .
        
-       CHECK-DELETE-MVT.
+       22300-CHECK-DELETE-MVT.
            IF (MAT-A3 = MAT-MVT)
-             perform DELETE-ASSURES-OUT
+             perform 22310-DELETE-ASSURES-OUT
            ELSE
              MOVE 4 TO ERR-CODE
-             perform CALL-ANO-PGM
+             perform 22700-CALL-ANO-PGM-ANO
            END-IF
            .
+           
 
-       WRITE-ASSURES-NO-MVT.
+       21000-WRITE-ASSURES-NO-MVT.
            write assures-out from e-assures-in
            
            ADD 1 TO CPT-ASSURES-OUT
            .
        
-       WRITE-ASSURES-OUT.
+       22110-WRITE-ASSURES-OUT.
            MOVE MAT-MVT TO MAT-A4 
            MOVE NOM-PRE-MVT TO NOM-PRE-A4 
            MOVE RUE-MVT TO RUE-A4
@@ -275,7 +417,7 @@
            ADD 1 TO CPT-CREATE
            .
        
-       REWRITE-ASSURES-OUT.
+       22210-REWRITE-ASSURES-OUT.
            MOVE MAT-MVT TO MAT-A4 
            MOVE NOM-PRE-MVT TO NOM-PRE-A4 
            MOVE RUE-MVT TO RUE-A4
@@ -290,24 +432,53 @@
            ADD 1 TO CPT-REWRITE
            .
 
-       DELETE-ASSURES-OUT.
+       22310-DELETE-ASSURES-OUT.
            ADD 1 TO CPT-DELETE
            .
 
        30000-END-PGM.
-           close f-assures-in f-assures-out f-mvt
-           perform DISPLAY-STATS
+           perform write-ano-footer
+           close f-assures-in f-assures-out f-mvt f-etatano
+           perform 31000-DISPLAY-STATS
+
+           CALL ano-pgm USING
+                BY VALUE 0
+                BY CONTENT FLAG-CLOSE
+                BY VALUE space
            STOP RUN
            .
 
-       DISPLAY-STATS.
+       31000-DISPLAY-STATS.
            perform VARYING I FROM 1 by 1 until I > 4
               ADD CPT-ANO-T(I) TO CPT-ANOMALIE 
            END-PERFORM
+           
+           perform 31100-DISPLAY-STATS-HEADER
 
            perform VARYING I from 5 by 1 until I > 15
-              MOVE I to error-key-9
-              read f-error
-              DISPLAY err-message CPT-STATS(I)
+              perform 31700-CALL-ANO-PGM-STATS
+              MOVE CPT-STATS(I) TO CPTI-F
+              DISPLAY FORMAT-ENRGMT-STATS
            END-PERFORM
+
+           perform 31200-DISPLAY-STATS-FOOTER
+           .
+
+       31100-DISPLAY-STATS-HEADER.
+           MOVE CORRESPONDING DATE-F TO FORMAT-HEADER-STATS-1
+           MOVE weekday(WEEKDAY-F) TO WEEKDAY-S 
+           MOVE CORRESPONDING TIME-F TO FORMAT-HEADER-STATS-2
+
+           DISPLAY FORMAT-HEADER-STATS-1
+           DISPLAY FORMAT-HEADER-STATS-2
+           DISPLAY FORMAT-EMPTY-LIGNE-ANO 
+           DISPLAY FORMAT-LIGNE-TABLE-STATS
+           DISPLAY FORMAT-TITLE-STATS
+           DISPLAY FORMAT-LIGNE-TABLE-STATS 
+           DISPLAY FORMAT-EMPTY-LIGNE-STATS
+           .
+
+       31200-DISPLAY-STATS-FOOTER.
+           DISPLAY FORMAT-EMPTY-LIGNE-STATS
+           DISPLAY FORMAT-LIGNE-TABLE-STATS
            .
